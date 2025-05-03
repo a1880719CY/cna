8bae18687a0a885dc429e914e9481bf26dfdcd9b
a1880719CY <a1880719@adelaide.edu.au>
2025-05-04 07:13:26 +0930

    fixed some bug

--- sr.c
+++ sr.c
@@ -167,6 +167,12 @@ void A_timerinterrupt(void)
   if (TRACE > 0){
     printf("----A: time out,resend packets!\n");
 
+    if (TRACE > 0){
+
+      printf ("---A: resending packet %d\n", buffer[windowfirst].seqnum);
+
+    }
+
     tolayer3(A,buffer[(windowfirst)]);
     packets_resent++;
 
